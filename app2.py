25678820bf6b3c3d95ed6555719eb125f4f3a2e6
alexiadb <49482351+alexiadb@users.noreply.github.com>
2022-03-11 07:58:45 +0100

    app2.py

--- app2.py
+++ app2.py
@@ -15,21 +15,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import LabelEncoder
 import streamlit as st
+import pickle
 
-
-
-
-
-
-
-
-string = "Prediction de salaire g2"
-st.set_page_config(page_title=string, page_icon="💰")
-
-
+df = pd.read_csv('df_final.csv')
 df_france = pd.read_csv('departements-france.csv')
-df_france.head()
-df = pd.read_csv('France_propre_V2.csv')
 
 
 
@@ -40,47 +29,44 @@ options = st.multiselect(
      'Quel poste?',
      df['Poste'].unique())
 
+
+
 #st.write('You selected:', options)
 options2 = st.multiselect(
-     'Quelle Département?',
-     df['Departement'].unique())
+     'Quel Département?',
+     np.sort(df['Departement'].unique()))
 
 
-options1 = st.multiselect(
-     'Quelle Région?',
-     df['Region'].unique())
+
 
 optionContract = st.multiselect(
      'Type de contrat?',
      df['Typedecontrat'].unique())
 
-labelencoder = LabelEncoder()
-df['Typedecontrat']=labelencoder.fit_transform(df['Typedecontrat'])
-#print(df['Typedecontrat'])
 
-df['Poste']=labelencoder.fit_transform(df['Poste'])
-#print(df['Poste'])
 
-'''
+loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
 
-X = df['Poste'].iloc[:,[0]].values
-y = df['Salaire'].iloc[:,-1].values
-print(y)
 
-X_train, X_test, y_train , y_test = train_test_split(X,y, test_size = 0.3 , random_state =0)
+pred = pd.DataFrame( {'Poste': options,
+             'Typedecontrat': optionContract,
+             'Departement': options2})
 
-optionContract = st.multiselect(
-     'Type de contrat?',
-     df['Typedecontrat'].unique())
 
-#exp = st.sidebar.slider("Contract: ", )
 
-reg = LinearRegression()
 
-reg.fit(X_train, y_train)
-y_pred = reg.predict([[optionContract]])
 
-st.write('type de contract: ', optionContract)
-st.write('Salaire: ', y_pred)
+y_pred = loaded_model.predict(pred)
+print(y_pred)
 
-'''
+st.markdown(f'<h1 style="color:#169408;font-size:32px;">{"Salaire: "}</h1>', unsafe_allow_html=True)
+st.write(round(y_pred[0],-1))
+
+
+
+
+##df_france.head()
+
+
+
+
